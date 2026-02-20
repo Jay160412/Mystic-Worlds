a26c001e702db842edfb6b68ed5e867609d542c2
Jay160412 <Anonym160412@gmail.com>
2026-02-20 21:38:58 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Game from '@/components/game/Game'
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen overflow-hidden" style={{ background: '#0B0E1A' }}>
+      <Game />
+    </main>
+  )
+}
